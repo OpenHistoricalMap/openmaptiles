b9b0786a8cf86b01f2237ed7d8bb7ba3f93cbd96
GreenInfo <gin@greeninfo.org>
2019-09-06 16:01:45 -0700

    #59 islands now get to stay polygons

--- layers/place/update_island_polygon.sql
+++ layers/place/update_island_polygon.sql
@@ -1,51 +1,19 @@
-DROP TRIGGER IF EXISTS trigger_flag ON osm_island_polygon;
-DROP TRIGGER IF EXISTS trigger_refresh ON place_island_polygon.updates;
+-- OMT had these, which force island polygons back to point, which loses their geometry!
+-- CREATE OR REPLACE FUNCTION update_osm_island_polygon()
+-- CREATE OR REPLACE FUNCTION place_island_polygon.flag()
+-- CREATE OR REPLACE FUNCTION place_island_polygon.refresh()
 
--- etldoc:  osm_island_polygon ->  osm_island_polygon
-CREATE OR REPLACE FUNCTION update_osm_island_polygon() RETURNS VOID AS $$
-BEGIN
-  UPDATE osm_island_polygon  SET geometry=ST_PointOnSurface(geometry) WHERE ST_GeometryType(geometry) <> 'ST_Point';
+-- so we went back and deleted these, then removed them from this file
+-- DROP TRIGGER IF EXISTS trigger_flag ON osm_island_polygon;
+-- DROP TRIGGER IF EXISTS trigger_flag ON backup.osm_island_polygon;
+-- DROP TRIGGER IF EXISTS trigger_refresh ON place_island_polygon.updates;
+-- DROP FUNCTION IF EXISTS update_osm_island_polygon();
+-- DROP FUNCTION IF EXISTS place_island_polygon.refresh();
+-- DROP FUNCTION IF EXISTS place_island_polygon.flag();
 
-  UPDATE osm_island_polygon
-  SET tags = update_tags(tags, geometry)
-  WHERE COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL;
-
-  ANALYZE osm_island_polygon;
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT update_osm_island_polygon();
-
--- Handle updates
-
-CREATE SCHEMA IF NOT EXISTS place_island_polygon;
-
-CREATE TABLE IF NOT EXISTS place_island_polygon.updates(id serial primary key, t text, unique (t));
-CREATE OR REPLACE FUNCTION place_island_polygon.flag() RETURNS trigger AS $$
-BEGIN
-    INSERT INTO place_island_polygon.updates(t) VALUES ('y')  ON CONFLICT(t) DO NOTHING;
-    RETURN null;
-END;
-$$ language plpgsql;
-
-CREATE OR REPLACE FUNCTION place_island_polygon.refresh() RETURNS trigger AS
-  $BODY$
-  BEGIN
-    RAISE LOG 'Refresh place_island_polygon';
-    PERFORM update_osm_island_polygon();
-    DELETE FROM place_island_polygon.updates;
-    RETURN null;
-  END;
-  $BODY$
-language plpgsql;
-
-CREATE TRIGGER trigger_flag
-    AFTER INSERT OR UPDATE OR DELETE ON osm_island_polygon
-    FOR EACH STATEMENT
-    EXECUTE PROCEDURE place_island_polygon.flag();
-
-CREATE CONSTRAINT TRIGGER trigger_refresh
-    AFTER INSERT ON place_island_polygon.updates
-    INITIALLY DEFERRED
-    FOR EACH ROW
-    EXECUTE PROCEDURE place_island_polygon.refresh();
+-- we do want to expose islands to the tile service, though
+-- see water.sql where we integrate these into the water layer, so match the water fields here
+CREATE OR REPLACE VIEW islands_for_tiles AS (
+    SELECT osm_id, geometry, 'island'::text AS class, name, start_date, end_date
+    FROM osm_island_polygon
+);
